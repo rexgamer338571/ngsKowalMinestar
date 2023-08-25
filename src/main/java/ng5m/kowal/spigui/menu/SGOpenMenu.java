b85a0ddee4a24765805a96aab71bf7423249ea13
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-25 16:05:25 +0200

    Create SGOpenMenu.java

---
+++ src/main/java/ng5m/kowal/spigui/menu/SGOpenMenu.java
@@ -0,0 +1,34 @@
+package ng5m.kowal.spigui.menu;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Used to refer to an open menu.
+ */
+public class SGOpenMenu {
+
+    private final SGMenu gui;
+    private final Player player;
+
+    public SGOpenMenu(SGMenu gui, Player player) {
+        this.gui = gui;
+        this.player = player;
+    }
+
+    /**
+     * Get the open {@link SGMenu} instance.
+     * @return The menu that is open.
+     */
+    public SGMenu getMenu() {
+        return this.gui;
+    }
+
+    /**
+     * Get the player viewing the {@link SGMenu}.
+     * @return The player viewing the menu.
+     */
+    public Player getPlayer() {
+        return this.player;
+    }
+
+}
