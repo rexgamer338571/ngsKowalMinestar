87e79b0350f9716d6625f754466052b5f254f2c9
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-25 16:01:35 +0200

    Create GUIUtils.java

---
+++ src/main/java/ng5m/kowal/GUIUtils.java
@@ -0,0 +1,45 @@
+package ng5m.kowal;
+
+import ng5m.boxpvppluginpack.util.Lore;
+import ng5m.boxpvppluginpack.spigui.buttons.SGButton;
+import ng5m.boxpvppluginpack.spigui.item.ItemBuilder;
+import ng5m.boxpvppluginpack.spigui.menu.SGMenu;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class GUIUtils {
+  public static void spawnPane(SGMenu menu, String color) {
+    menu.addButton(new SGButton(new ItemBuilder(Material.valueOf(color+"_STAINED_GLASS_PANE")).name("&0 ").build()));
+  }
+
+  public static void addAnotherItem(SGMenu menu, Material material, String name, String[] lore, Inventory openLater, int panes) {
+    for (int i = 0; i<panes; i++) {
+      SGButton btn = new SGButton(
+        new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).name("&0 ").build()
+      );
+      menu.addButton(btn);
+    }
+
+    SGButton btn2 = new SGButton(
+      new ItemBuilder(material).name(name).lore(lore).build()
+    ).withListener((InventoryClickEvent e) -> {
+      e.getWhoClicked().openInventory(openLater);
+    });
+    menu.addButton(btn2);
+  }
+
+  public static void addItemNoListener(SGMenu menu, Material material, String name, String[] lore, int panes) {
+    for (int i = 0; i<panes; i++) {
+      SGButton btn = new SGButton(
+        new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).name("&0 ").build()
+      );
+      menu.addButton(btn);
+    }
+    SGButton btn2 = new SGButton(
+      new ItemBuilder(material).name(name).lore(lore).build()
+    );
+    menu.addButton(btn2);
+  }
+}
