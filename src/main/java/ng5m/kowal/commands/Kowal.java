c4aced12eb77d008878bcf0788223c28821fd902
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-08-25 16:19:58 +0200

    Create Kowal.java

---
+++ src/main/java/ng5m/kowal/commands/Kowal.java
@@ -0,0 +1,19 @@
+package ng5m.kowal.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Kowal implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (sender instanceof Player) {
+      Player p = (Player) sender;
+      
+      p.openInventory(KowadloInv.getInv().getInventory());
+    }
+    return true;
+  }
+}
